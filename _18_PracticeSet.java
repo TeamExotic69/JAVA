5aa263605db6a7e198068a86f2b4560316748200
TeamExotic69 <149905379+TeamExotic69@users.noreply.github.com>
2024-01-27 00:59:24 +0530

    Add files via upload

---
+++ _18_PracticeSet.java
@@ -0,0 +1,128 @@
+package com.company;
+import com.sun.nio.sctp.SctpStandardSocketOptions;
+
+import java.util.Scanner;
+public class _18_PracticeSet {
+    public static void main(String[] args) {
+        //Q1
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        short a=sc.nextShort();
+//        for(int i =1;i<=a;i++){
+//            for(int j=a;j>=i;j--){
+//            System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
+
+        //Q2
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        int a=sc.nextInt();
+//        int n=0;
+//        int sum=0,p=0;
+//        while(p<=a){
+//            if(n%2==0) {
+//                sum += n;
+//                n+=2;
+//            }
+//            else n++;
+//            p++;
+//        }
+//        System.out.print("This is sum of first n Even numbers:"+sum);
+
+
+
+        //Q3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        int t=sc.nextInt();
+//        for(int i=1;i<=10;i++){
+//            System.out.println(t+" X "+i+" = "+t*i);
+//        }
+
+
+
+        //Q4
+//        short n=10;
+//        for(int i =10;i>=1;i--){
+//            System.out.println(n+" X "+i+" = "+n*i);
+//        }
+
+
+
+        //Q5
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        short a=sc.nextShort();
+//        int factorial=1;
+//        for(int i=a;i>=1;i--){
+//            factorial*=i;
+//        }
+//        System.out.print("Factorial is: "+factorial);
+
+
+        //Q6
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        short a=sc.nextShort();
+//        int factorial=1;
+//        while(a!=0){
+//            factorial*=a;
+//            a--;
+//        }
+//        System.out.print("Factorial is: "+factorial);
+
+
+        //Q7
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        short a=sc.nextShort();
+//        int t;
+//        while(a!=0){
+//            t=a;
+//            while(t!=0){
+//                System.out.print("*");
+//                t--;
+//            }
+//            a--;
+//            System.out.println();
+//        }
+
+
+
+        //Q8
+//        int table,sum=0;
+//        for(int i=1;i<=10;i++){
+//            table=i*8;
+//            sum+=table;
+//        }
+//        System.out.print("Sum of number occuring in table of 8 are:"+sum);
+
+
+
+        //Q9
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter n:");
+//        int a=sc.nextInt();
+//        int n=0;
+//        int sum=0,p=0;
+//        for(int i = 0; i<=Integer.MAX_VALUE ; i++){
+//            if(i%2==0){
+//                sum+=i;
+//                p++;
+//            }
+//            if(p==a) break;
+//        }
+//        System.out.print("This is sum of first n Even numbers:"+sum);
+    }
+}
+
+
+
+
+
+
+
